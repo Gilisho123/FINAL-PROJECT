18986adef85626237c7c055ac0a85e5077199bd3
Gilisho Leteipa <leteipagilisho@gmail.com>
2025-10-03 09:33:07 +0300

    Delete about.js

--- about.js
+++
@@ -1,7 +0,0 @@
-// Simple animation for heading
-document.addEventListener("DOMContentLoaded", () => {
-  const heading = document.querySelector("h2");
-  heading.style.opacity = 0;
-  heading.style.transition = "opacity 1.5s";
-  setTimeout(() => heading.style.opacity = 1, 300);
-});
